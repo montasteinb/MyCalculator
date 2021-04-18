43562697881d1ed671d17e597c26d9c5d0739061
Monta <79810291+montasteinb@users.noreply.github.com>
2021-04-18 19:29:34 +0300

    Add files via upload

---
+++ Calculator/src/main/java/Calculator.java
@@ -0,0 +1,27 @@
+public class Calculator {
+
+    public static int add;
+    public static int subtract;
+    public static int multiply;
+    public static int divide;
+
+    public static int add(int num1, int num2){
+        add = num1 + num2;
+        return add;
+    }
+    public static int subtract(int num1, int num2){
+        subtract = num1 - num2;
+        return subtract;
+    }
+    public static int multiply(int num1, int num2){
+        multiply = num1 * num2;
+        return multiply;
+    }
+    public static int divide(int num1, int num2){
+        divide = num1 / num2;
+        return divide;
+    }
+
+
+
+}
